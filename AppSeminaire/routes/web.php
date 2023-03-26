8bab8fbc20a115f3ff39c491e141d7fa9aa812fa
Don Makila <makiladon14@gmail.com>
2023-03-26 15:25:05 -0800

    Intégration du template AdminLTE

--- AppSeminaire/routes/web.php
+++ AppSeminaire/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin',[App\Http\Controllers\AdminController::class,'dashboard']);
 
 Auth::routes();
 
