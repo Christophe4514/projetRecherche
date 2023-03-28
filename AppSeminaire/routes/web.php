d286017768969056c9a2f5974321f13a698c7412
jean <christophewizamo@gmail.com>
2023-03-28 11:23:12 +0100

    directory client

--- AppSeminaire/routes/web.php
+++ AppSeminaire/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ClientController::class, 'index']);
 
 Auth::routes();
 
